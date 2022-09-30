2724f85f3ca679767be85d68002800276f65faa4
rkorostin <rkorostin@gmail.com>
2022-09-30 18:36:54 +0300

    Exp_005 Оператор if

---
+++ Exp_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+//if(username == "роман") // На точное совпадение
+if(username.ToLower() == "роман")
+{
+    Console.WriteLine("Здорово, Роман!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
