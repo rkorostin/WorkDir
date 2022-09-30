84d1eb9884b3580dd397ebe58c1f95d1ef271120
rkorostin <rkorostin@gmail.com>
2022-09-30 18:26:52 +0300

    Exp_004 Сумма рандомных чисел

---
+++ Exp_004/Program.cs
@@ -0,0 +1,7 @@
+﻿double numA = new Random().Next (1,10); // 1 2 3 .. 9
+double numB = new Random().Next (1,10); // 1 2 3 .. 9
+double result = numB + numA;
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+Console.Write("Сумма чисел: ");
+Console.Write(result);
