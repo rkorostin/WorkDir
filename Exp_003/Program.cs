93d82cec054d5411acf47d774000ed48e70dc601
rkorostin <rkorostin@gmail.com>
2022-09-30 18:19:41 +0300

    Exp_003 Частное чисел

---
+++ Exp_003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Частное чисел:");
+// int numberA = 10; int - целое число
+// double numberB = 5; double - вещественное число 
+double numberA = 10;
+double numberB = 4;
+Console.WriteLine(numberA / numberB);
