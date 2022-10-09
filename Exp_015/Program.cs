ed5c9ba944445734758bc329a3dd80e4b73d8608
rkorostin <rkorostin@gmail.com>
2022-10-09 18:08:07 +0300

    Exp_015. Пример задачи с заменой в тексте символа

---
+++ Exp_015/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Дан текст. В тексте нужно все пробелы заменить черточками,
+маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+маленькими “с”.
+*/
+
+string text = "Пример текста, в котором нужно заменить все пробелы на символ *";
+
+string Replase(string text, char oldValue, char newValue);
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replase(text, ' ', '*');
+ConsoleWrite(newText);
+
