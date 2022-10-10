8875329d1e2a662489840c9b1410ad4105cbfb7a
rkorostin <rkorostin@gmail.com>
2022-10-10 10:55:36 +0300

    Доп вспомогательный механизм, который ищет максимальное из трёх чисел

--- Exp_009/Program.cs
+++ Exp_009/Program.cs
@@ -33,7 +33,6 @@ int[] array = { 9, 28, 1, 32, 1990, 34, 23, 33, 9999 }; // определяем 
 //Т.е. одной строкой (32-ой) записали с 18 по 26 строки
 array[0] = 333; // присвоил первому элементу массива значение 333
 //Console.WriteLine(array[0]);
-
 /*
 int max1 = FunctionFindMax(a1, a2, a3);  // Поиск max1 из первых трех чисел
 int max2 = FunctionFindMax(b1, b2, b3);  // Поиск max2 
@@ -42,7 +41,6 @@ int max3 = FunctionFindMax(c1, c2, c3);  // Поиск max3
 int max = FunctionFindMax(FunctionFindMax(a1, a2, a3), FunctionFindMax(b1, b2, b3), FunctionFindMax(c1, c2, c3));
 Console.WriteLine(max);
 */
-
 int max = FunctionFindMax(FunctionFindMax(array[0], array[1], array[2]),
          FunctionFindMax(array[3], array[4], array[5]),
          FunctionFindMax(array[6], array[7], array[8]));
