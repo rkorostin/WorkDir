77e799a25fc9a4821873aae44db8e47aeaa14e96
rkorostin <rkorostin@gmail.com>
2022-10-20 10:51:32 +0300

    Факториял через рекурсию

---
+++ Exp_018/Program.cs
@@ -0,0 +1,15 @@
+﻿// Факториал при помощи рекурсии
+// 5!=5*4*3*2*1
+//    5*4!
+//      4*3!
+//        3*2!
+//          2*1!
+
+int Factorial(int n)
+{   //1! = 1, 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(5));
+
+
